a60c90720baf2b9f7fca4ed71afc2a18ac3ff3a9
dabitsChile <dcastros@gmail.com>
2022-02-13 02:47:40 -0300

    com modulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -29,9 +29,8 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [],
   imports: [
     RouterModule.forRoot( routes )
-  ]
+  ], exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/pages/pages/pages.component.ts
+++ src/app/pages/pages/pages.component.ts
@@ -3,11 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
-  template: `
-    <p>
-      pages works!
-    </p>
-  `,
   styles: [
   ]
 })
