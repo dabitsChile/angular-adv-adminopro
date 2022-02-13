c080fef7808f6b96ed11babeedfdefc32600349d
dabitsChile <dcastros@gmail.com>
2022-02-13 01:39:09 -0300

    carga inicial

---
+++ src/app/pages/pages/pages.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+  
+@Component({
+  selector: 'app-pages',
+  templateUrl: './pages.component.html',
+  template: `
+    <p>
+      pages works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class PagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
